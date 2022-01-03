495f8e6f3f16bbe11d1eabe3a6cc90a20c6604cf
Matthew Flegg <matthewflegg@outlook.com>
2022-01-03 18:25:11 +0000

    fixed the error with the form validation script not detecting if the job role field hadn't been selected

--- Scripts/validateform.js
+++ Scripts/validateform.js
@@ -1,10 +1,12 @@
 function validateForm() {
+    var formElements = document.getElementById('application-form').elements;
+
     // For each form element, check if it's left blank. If it is, show a message and return false
     for (var i = 0; i < document.getElementById('application-form').elements.length; i++) {
-        if (document.getElementById('application-form').elements[i].value == "") {
+        if (formElements[i].value == "" || formElements[i].options[formElements[i].selectedIndex].value == "None") {
 
             // Get the 'name' attribute of the form element and print ("{name of element} must be filled out.")
-            alert(document.getElementById('application-form').elements[i].getAttribute("name") + " must be filled out.") 
+            alert(formElements[i].getAttribute("name") + " must be filled out.") 
             return false;
         }
     }
