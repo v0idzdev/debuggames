e33d5f8b4a0011b41f2f12c1f7dd57d8bf5e0531
Matthew Flegg <matthewflegg@outlook.com>
2022-01-03 19:48:15 +0000

    fully fixed form validation + refactored code

--- Scripts/validateform.js
+++ Scripts/validateform.js
@@ -1,17 +1,37 @@
-function validateForm() {
-    var formElements = document.getElementById('application-form').elements;
+/* **** OLD FORM VALIDATION CODE ****
 
+function validateForm() {
     // For each form element, check if it's left blank. If it is, show a message and return false
-    for (var i = 0; i < document.getElementById('application-form').elements.length; i++) {
-        if (formElements[i].value == "" || formElements[i].options[formElements[i].selectedIndex].value == "None") {
+    for (var i = 1; i < document.getElementById('application-form').elements.length; i++) {
+        if (document.getElementById('application-form').elements[i].value == "") {
 
             // Get the 'name' attribute of the form element and print ("{name of element} must be filled out.")
-            alert(formElements[i].getAttribute("name") + " must be filled out.") 
+            alert(document.getElementById('application-form').elements[i].getAttribute("name") + " must be filled out.") 
             return false;
         }
     }
 
     // Else return true, and submit
+    alert("The form was sumbitted sucessfully.")
+    return true;
+}*/
+
+function validateForm() {
+    let fields = [].slice.call(document.getElementById('application-form').elements);
+    let select = fields.shift();
+    
+    if (select.options[select.selectedIndex].value == "None") {
+        alert("Job role must be filled out.");
+        return false;
+    }
+
+    let isValid = fields.map((field) => field.value == "" ? false : true)
+
+    if (isValid.includes(false)) {
+        alert(`${fields[isValid.indexOf(false)].getAttribute("name")} must be filled out.`);
+        return false;
+    }
+
     alert("The form was sumbitted sucessfully.")
     return true;
 }
