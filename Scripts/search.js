1721357028a40dbaa06f5b8b03a6f4f511b1c490
20227397 - Matthew Flegg <20227397@shrewsbury.ac.uk>
2022-01-05 12:31:05 +0000

    added: closing of search bar when unfocused

--- Scripts/search.js
+++ Scripts/search.js
@@ -32,6 +32,49 @@ function searchBar() {
     options.every(optionNotVisible) && input != "" ? noMatch.style.display = "list-item" : noMatch.style.display = "none"
 }
 
+/* // Search bar on focus out event 
+function onSearchBarFocusLoss() {
+    const hideOption = (option) => option.style.display = none;
+    const linkClicked = (links) => links.clicked == true ? hasClickedLink = true : hasClickedLink = false
+
+    let options = [].slice.call(document.getElementsByClassName('search-bar-option'));
+
+    
+} */
+
+/* let options = document.querySelectorAll('.search-bar-option');
+
+options.forEach(option => { // Detects whether a link has been clicked, if it has, keep showing the options
+    option.addEventListener('click', (event) => { [].slice.call(options).map((option) => option.style.display = "list-item"); });
+}); 
+
+// Detects whether the search bar input has been unfocused - if it has, hide the options
+document.getElementById('search-bar-input').addEventListener('focusout', (event) => {
+    [].slice.call(options).map((option) => option.style.display = "none");
+}); */
+
+// if anything other than the search input has been clicked on
+// check if it's got the class name '.search-option-link'
+// if it has, DON'T hide the links
+// else, hide them 
+
+let links = document.getElementsByClassName('search-option-link');
+let options = [].slice.call(document.getElementsByClassName('search-bar-option'));
+
+const showOptions = (option) => option.style.display = "list-item";
+const hideOptions = (option) => option.style.display = "none";
+
+document.addEventListener('click', (event) => {
+    options.map(event.target.matches('.search-option-link') ? showOptions : hideOptions);
+});
+
+
+/* // If search bar link is clicked on, hide search bar options
+function onSearchOptionClick() {
+    options = [].slice.call(document.getElementsByClassName('search-bar-option'));
+    options.map((option) => option.style.display = "none");
+} */
+
 /* *** HIDES SEARCH OPTIONS WHEN SEARCH BAR ISN'T FOCUSED ON ***
 function onSearchBarFocusLoss() {
     let searchOptions = document.getElementsByClassName('search-bar-option');
